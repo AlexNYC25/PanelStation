2edfea3ce72d2631e90ea80814c9397b891d5ce6
Alexis Montes <montes.alexis25@gmail.com>
2024-11-14 01:53:02 -0500

    Updated start up script to check for folder table

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 // index.js
 import app from './src/app.js';
-import { checkComicBookDataDirectoryExists, checkAndCreateComicBookTable } from './src/startup.js';
+import { checkComicBookDataDirectoryExists, checkAndCreateComicBookTable, checkAndCreateComicFolderTable } from './src/startup.js';
 import { connectToDatabase } from './src/config/dbConnection.js';
 
 const PORT = process.env.PORT || 3000;
 
 connectToDatabase();
 checkComicBookDataDirectoryExists();
+
 checkAndCreateComicBookTable();
+checkAndCreateComicFolderTable();
 
 // Start the server
 app.listen(PORT, () => {
--- src/startup.js
+++ src/startup.js
@@ -1,31 +1,31 @@
-import fs from 'fs';
+import fs from "fs";
 
-import { runQuery } from './config/dbConnection.js';
+import { runQuery } from "./config/dbConnection.js";
 
 let checkComicBookDataDirectoryExists = () => {
-    const dataDir = process.env.DATA_DIR;
-    if (!fs.existsSync(dataDir)) {
-        try {
-            fs.mkdirSync(dataDir);
-            console.log(`Directory ${dataDir} created successfully.`);
-        } catch (err) {
-            console.error(`Error creating directory ${dataDir}:`, err);
-        }
-    } else {
-        console.log(`Directory ${dataDir} already exists.`);
+  const dataDir = process.env.DATA_DIR;
+  if (!fs.existsSync(dataDir)) {
+    try {
+      fs.mkdirSync(dataDir);
+      console.log(`Directory ${dataDir} created successfully.`);
+    } catch (err) {
+      console.error(`Error creating directory ${dataDir}:`, err);
     }
-}
+  } else {
+    console.log(`Directory ${dataDir} already exists.`);
+  }
+};
 
 const checkAndCreateComicBookTable = async () => {
-    const checkTableQuery = `
+  const checkTableQuery = `
       SELECT EXISTS (
         SELECT FROM information_schema.tables 
         WHERE table_schema = 'public' 
         AND table_name = 'comic_book'
       );
     `;
-  
-    const createTableQuery = `
+
+  const createTableQuery = `
       CREATE TABLE comic_book (
         id SERIAL PRIMARY KEY,
         file_name VARCHAR(255) NOT NULL,
@@ -34,20 +34,56 @@ const checkAndCreateComicBookTable = async () => {
         created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
       );
     `;
-  
-    try {
-      const result = await runQuery(checkTableQuery);
-      const tableExists = result[0].exists;
-  
-      if (!tableExists) {
-        await runQuery(createTableQuery);
-        console.log('comic_book table created successfully.');
-      } else {
-        console.log('comic_book table already exists.');
-      }
-    } catch (err) {
-      console.error('Error checking or creating comic_book table:', err);
+
+  try {
+    const result = await runQuery(checkTableQuery);
+    const tableExists = result[0].exists;
+
+    if (!tableExists) {
+      await runQuery(createTableQuery);
+      console.log("comic_book table created successfully.");
+    } else {
+      console.log("comic_book table already exists.");
     }
+  } catch (err) {
+    console.error("Error checking or creating comic_book table:", err);
+  }
 };
 
-export { checkComicBookDataDirectoryExists, checkAndCreateComicBookTable };
+const checkAndCreateComicFolderTable = async () => {
+  const checkTableQuery = `
+    SELECT EXISTS (
+      SELECT FROM information_schema.tables 
+      WHERE table_schema = 'public' 
+      AND table_name = 'comic_folder'
+    );
+  `;
+
+  const createTableQuery = `
+    CREATE TABLE comic_folder (
+        id SERIAL PRIMARY KEY,
+        folder_path TEXT NOT NULL UNIQUE,
+        folder_hash TEXT NOT NULL UNIQUE
+    );
+  `;
+
+  try {
+    const result = await runQuery(checkTableQuery);
+    const tableExists = result[0].exists;
+
+    if (!tableExists) {
+      await runQuery(createTableQuery);
+      console.log("comic_folder table created successfully.");
+    } else {
+      console.log("comic_folder table already exists.");
+    }
+  } catch (err) {
+    console.error("Error checking or creating comic_folder table:", err);
+  }
+};
+
+export {
+  checkComicBookDataDirectoryExists,
+  checkAndCreateComicBookTable,
+  checkAndCreateComicFolderTable,
+};
