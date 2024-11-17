3e2ddd83ab77db91575ddf0ff039514e889ca109
Alexis Montes <montes.alexis25@gmail.com>
2024-11-16 23:42:10 -0500

    updated index.js to call functions in order to create tables when ready and get server to listen

--- index.js
+++ index.js
@@ -1,18 +1,35 @@
 // index.js
-import app from './src/app.js';
-import { checkComicBookDataDirectoryExists, checkAndCreateComicBookTable, checkAndCreateComicFolderTable, checkAndCreateComicSeriesTable } from './src/startup.js';
-import { connectToDatabase } from './src/config/dbConnection.js';
+import app from "./src/app.js";
+import {
+  checkComicBookDataDirectoryExists,
+  checkAndCreateComicBookTable,
+  checkAndCreateComicFolderTable,
+  checkAndCreateComicSeriesTable,
+  checkAndCreateComicSeriesFoldersTable,
+  checkAndCreateComicBookRolesTable,
+  checkAndCreateComicBookMetadataTable
+} from "./src/startup.js";
+import { connectToDatabase } from "./src/config/dbConnection.js";
 
 const PORT = process.env.PORT || 3000;
 
 connectToDatabase();
 checkComicBookDataDirectoryExists();
 
-checkAndCreateComicBookTable();
-checkAndCreateComicFolderTable();
-checkAndCreateComicSeriesTable();
+const initializeDatabase = async () => {
+  await checkAndCreateComicBookTable();
+  await checkAndCreateComicFolderTable();
+  await checkAndCreateComicSeriesTable();
+  await checkAndCreateComicSeriesFoldersTable();
+  await checkAndCreateComicBookRolesTable();
+  await checkAndCreateComicBookMetadataTable();
+};
 
-// Start the server
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+initializeDatabase().then(() => {
+  // Start the server after the database initialization is complete
+  app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+  });
+}).catch((err) => {
+  console.error('Error during database initialization:', err);
 });
