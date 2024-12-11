999ea782b2d91f477a9e16c2080a9015b2b0b34f
Alexis Montes <montes.alexis25@gmail.com>
2024-12-11 02:21:33 -0500

    Refactor XML utility functions: enhance metadata extraction and improve role parsing logic

--- src/utilities/stringTools.js
+++
@@ -1,6 +0,0 @@
-export const extractFloatingPointNumbers = (inputString) => {
-  // Regular expression to match floating point numbers
-  const regex = /-?\d+(\.\d+)?/g;
-  const matches = inputString.match(regex);
-  return matches ? matches.join(" ") : "";
-};
--- src/utilities/xmlTools.js
+++ src/utilities/xmlTools.js
@@ -1,21 +1,34 @@
+import { isObject, isArray } from "lodash";
 import { parseStringPromise } from "xml2js";
 
+/*
+ * This function takes a string of XML text and converts it to a JSON object.
+ * @param {string} xmlText - The XML text to convert to JSON.
+ * @returns {Promise<object>} - A promise that resolves to a JSON object.
+ * @throws {Error} - An error if the conversion fails.
+ */
 export const convertComicInfoXmlTextToJson = async (xmlText) => {
   try {
-    const result = await parseStringPromise(xmlText);
-    return result;
+    const xmlObject = await parseStringPromise(xmlText);
+    return xmlObject;
   } catch (err) {
     console.error("Error converting comicinfo.xml text to JSON:", err);
     throw err;
   }
 };
 
+/*
+ * This function takes a JSON object and prints out the key-value pairs.
+ * @param {object} comicInfoXmlObj - The JSON object to print out.
+ * @returns {void}
+ * @throws {Error} - An error if the object is not valid.
+ */
 export const parseAndPrintOutComicInfoXml = async (comicInfoXmlObj) => {
   for (const [key, value] of Object.entries(comicInfoXmlObj.ComicInfo)) {
-    const isValueArray = Array.isArray(value);
-    const isValueObject = typeof value === "object";
+    const isValueArray = isArray(value);
+    const isValueObject = isObject(value);
     const isArrayOfObjects =
-      isValueArray && value.every((item) => typeof item === "object");
+      isValueArray && value.every((item) => isObject(item));
 
     if (isValueObject) {
       console.log(`${key}: ${JSON.stringify(value)}`);
@@ -30,58 +43,41 @@ export const parseAndPrintOutComicInfoXml = async (comicInfoXmlObj) => {
   }
 };
 
+/*
+ * This function takes a JSON object and returns the metadata.
+ * @param {object} comicInfoXmlObj - The JSON object to parse.
+ * @returns {Promise<object>} - A promise that resolves to the metadata.
+ * @throws {Error} - An error if the object is not valid.
+ */
 export const parseComicInfoXmlForMetadata = async (comicInfoXmlObj) => {
-  const metadata = {
-    series_name: null,
-    title: null,
-    issue_number: null,
-    publisher: null,
-    publication_date: null,
-    summary: null,
-    genre: null,
-    page_count: null,
-  };
-
   const comicInfo = comicInfoXmlObj.ComicInfo;
 
-  if (comicInfo.Series) {
-    metadata.series_name = comicInfo.Series[0];
-  }
+  let comicInfoMetadata = {
+    series_name: comicInfo.Series ? comicInfo.Series[0] : null,
+    title: comicInfo.Title ? comicInfo.Title[0] : null,
+    issue_number: comicInfo.Number ? comicInfo.Number[0] : null,
+    publisher: comicInfo.Publisher ? comicInfo.Publisher[0] : null,
+    publication_date:
+      comicInfo.Day && comicInfo.Month && comicInfo.Year
+        ? `${comicInfo.Month[0]}-${comicInfo.Day[0]}-${comicInfo.Year[0]}`
+        : null,
+    summary: comicInfo.Summary ? comicInfo.Summary[0] : null,
+    genre: comicInfo.Genre ? comicInfo.Genre[0] : null,
+    page_count: comicInfo.PageCount
+      ? comicInfo.PageCount[0]
+      : comicInfo.Pages[0].Page.length,
+  };
 
-  if (comicInfo.Title) {
-    metadata.title = comicInfo.Title[0];
-  }
-
-  if (comicInfo.Number) {
-    metadata.issue_number = comicInfo.Number[0];
-  }
-
-  if (comicInfo.Publisher) {
-    metadata.publisher = comicInfo.Publisher[0];
-  }
-
-  if (comicInfo.Day && comicInfo.Month && comicInfo.Year) {
-    metadata.publication_date = `${comicInfo.Month[0]}-${comicInfo.Day[0]}-${comicInfo.Year[0]}`;
-  }
-
-  if (comicInfo.Summary) {
-    metadata.summary = comicInfo.Summary[0];
-  }
-
-  if (comicInfo.Genre) {
-    metadata.genre = comicInfo.Genre[0];
-  }
-
-  if (comicInfo.PageCount) {
-    metadata.page_count = comicInfo.PageCount[0];
-  } else {
-    metadata.page_count = comicInfo.Pages[0].Page.length;
-  }
-
-  return metadata;
+  return comicInfoMetadata;
 };
 
-export const parseComicInfoXmlForRoles = async (comicInfoXmlObj) => {
+/*
+ * This function takes a JSON object and returns the roles.
+ * @param {object} comicInfoXmlObj - The JSON object to parse.
+ * @returns {Promise<object>} - A promise that resolves to the roles.
+ * @throws {Error} - An error if the object is not valid
+ */
+export const parseComicInfoXmlForRoles = (comicInfoXmlObj) => {
   const roles = [
     "Writer",
     "Inker",
@@ -90,31 +86,20 @@ export const parseComicInfoXmlForRoles = async (comicInfoXmlObj) => {
     "CoverArtist",
     "Editor",
   ];
-
   const comicInfo = comicInfoXmlObj.ComicInfo;
   const rolesArray = [];
 
   roles.forEach((role) => {
     if (comicInfo[role]) {
       comicInfo[role].forEach((value) => {
-        const values = value.split(',').map(v => v.trim());
-        values.forEach((val) => {
-          const roleObject = {};
-          if (role === "Writer") {
-            roleObject.writer = val;
-          } else if (role === "Inker") {
-            roleObject.inker = val;
-          } else if (role === "Colorist") {
-            roleObject.colorist = val;
-          } else if (role === "Letterer") {
-            roleObject.letterer = val;
-          } else if (role === "CoverArtist") {
-            roleObject.cover_artist = val;
-          } else if (role === "Editor") {
-            roleObject.editor = val;
-          }
-          rolesArray.push(roleObject);
-        });
+        value
+          .split(",")
+          .map((v) => v.trim())
+          .forEach((val) => {
+            const roleKey =
+              role === "CoverArtist" ? "cover_artist" : role.toLowerCase();
+            rolesArray.push({ [roleKey]: val });
+          });
       });
     }
   });
