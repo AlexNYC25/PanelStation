97881a1d376b4b442612995d638e2c1050fac7eb
Alexis Montes <montes.alexis25@gmail.com>
2025-01-19 02:23:11 -0500

    Added new function to parse for series name from the folder containing the comic book

--- src/utilities/comicFolderParser.js
+++ src/utilities/comicFolderParser.js
@@ -22,6 +22,26 @@ export const parseComicFolderNameForYear = (folderPath) => {
   const match = folderName.match(dateRegex);
 
   const seriesYear = match[0].replace('(', '').replace(')', '');
-  return { seriesYear };
+  return { seriesYear: parseInt(seriesYear, 10) };
+}
+
+export const parseComicFolderNameForName = (folderPath) =>{
+    // Get the last part of the folderPath (true folder name)
+    const folderName = folderPath.split(/\\|\//).pop();
+
+    // Check if the string contains "(" and get the substring up to it
+    const indexOfParen = folderName.indexOf("(");
+    if (indexOfParen !== -1) {
+        return folderName.substring(0, indexOfParen).trim();
+    }
+
+    // If no "(", look for a 4-digit sequence
+    const fourDigitMatch = folderName.match(/\d{4}/);
+    if (fourDigitMatch) {
+        return folderName.substring(0, fourDigitMatch.index).trim();
+    }
+
+    // If neither condition is met, return the full string
+    return folderName;
 }
 
