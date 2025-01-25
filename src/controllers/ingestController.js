c934d51c26a1423fd2add6a7bd30567776ecc4fd
Alexis Montes <montes.alexis25@gmail.com>
2025-01-25 03:09:48 -0500

    Updated api response to return summary object

--- src/controllers/ingestController.js
+++ src/controllers/ingestController.js
@@ -4,9 +4,9 @@ import {
 
 export const startIngest = async (req, res) => {
   try {
-    addFilesToDatabase();
+    const ingestSummary = await addFilesToDatabase();
 
-    res.status(200).send("Ingest complete");
+    res.status(200).send(ingestSummary);
   } catch (err) {
     console.error("Error adding folders and files to database:", err);
   }
