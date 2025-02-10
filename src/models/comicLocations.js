845d0ba7d2b3519ba3ff16174f28201399bec1e4
Alexis Montes <montes.alexis25@gmail.com>
2025-02-09 19:38:58 -0500

    removed descirption from comic_locations table, added insert function

--- src/models/comicLocations.js
+++ src/models/comicLocations.js
@@ -14,7 +14,7 @@ export const checkAndCreateComicLocationsTable = async () => {
     CREATE TABLE comic_locations (
       id SERIAL PRIMARY KEY,
       name VARCHAR(255) NOT NULL,
-      description TEXT
+      UNIQUE (name)
     );
   `;
 
@@ -43,5 +43,32 @@ export const deleteComicLocationsTable = async () => {
     logger.debug("comic_locations table deleted successfully.");
   } catch (err) {
     logger.error("Error deleting comic_locations table:", err);
+  }
+};
+
+export const insertComicLocationIntoDb = async (location) => {
+  const insertQuery = `
+    INSERT INTO comic_locations (name)
+    VALUES ($1)
+    ON CONFLICT (name) DO NOTHING
+    RETURNING id;
+  `;
+
+  const selectQuery = `
+    SELECT name FROM comic_locations WHERE name = $1;
+  `;
+
+  try {
+    const result = await runQuery(selectQuery, [location]);
+    if (result.length === 0) {
+      let insertResult = await runQuery(insertQuery, [location]);
+      logger.debug(`Inserted location ${location} into comic_locations.`);
+      return insertResult[0].id;
+    } else {
+      logger.debug(`Location ${location} already exists in comic_locations.`);
+      return result[0].id;
+    }
+  } catch (err) {
+    logger.error(`Error inserting location ${location} into comic_locations:`, err);
   }
 };
